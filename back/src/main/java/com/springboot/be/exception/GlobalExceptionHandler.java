27f892c483055ad23ea7b812731d60e55b27100e
ebinee <skyim0907@naver.com>
2025-09-09 17:54:12 +0900

    ãsave editing post api

--- back/src/main/java/com/springboot/be/exception/GlobalExceptionHandler.java
+++ back/src/main/java/com/springboot/be/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.springboot.be.exception;
 
 import com.springboot.be.dto.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -43,6 +45,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiResponse<?> handleException(Exception e) {
+        log.error("Unhandled exception", e);
         return ApiResponse.error(500, "서버 내부 오류가 발생했습니다.");
     }
 }
